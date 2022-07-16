b15392f58321096bb542a74fc9a9f02d61530395
Rajan <58392152+RajanGoyal1002@users.noreply.github.com>
2022-07-16 18:52:04 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -16,30 +16,9 @@ def app():
     if api_key:
             
         # Setting up the Title
-        st.title("🕹️ GPT-3 Sandbox")
-
-        st.write(f"""
-        ## Made with ❤️ for 🤖 by Kairos Data Labs
-        """)
-        st.write("_This Sandbox will help you create & deploy OpenAI API powered app with a few lines of Python_")
-        
-        st.write("---")
-
-        st.write(f"""
-        ### 🎨 How to create an example application
-        - To create an app with a basic flow, you can use our default template for [Email Generation App](https://github.com/Shubhamsaboo/kairos_gpt3/blob/master/GPT-3%20Sandbox/email_generation)
-        - This template has a flow that will work well for a couple of basic use-cases and you can use it just by replacing the default [training prompt](https://github.com/Shubhamsaboo/kairos_gpt3/blob/master/GPT-3%20Sandbox/email_generation/training_data.py) with your application specific prompt
-        - To test the application, provide a one-line task description in the -> **Input** text box
-        - You would then get an **Output** -> that should give you a good feeling of how to structure your application specific tasks
-        
-        ### 🙌 Customize your application & contribute 
-        - If you want to play with the code and [contribute](https://github.com/Shubhamsaboo/kairos_gpt3/blob/master/CONTRIBUTE.md) your application templates, please do! Sharing the knowledge with the GPT-3 community is the key mission of this sandbox ❤️
-        - To customize the UI of the web-application, please refer to the [Streamlit](https://docs.streamlit.io) 📖
-        """)
-
+        st.title("🕹️ Blog Generater ")
+        st.write(" This API powered app will generate text for blog ")
         st.write(f"""---""")
-
-        st.image("./ai.png", use_column_width=True)
 
         input = st.text_input('Input:')
 
